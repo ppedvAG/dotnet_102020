0134d6fc8eb7ef4da003fb6c1452032002a2a546
André <AndreR@ppedv.de>
2020-10-15 10:58:17 +0200

    NoSQL mit liteDB

---
+++ noSQL_liteDB/noSQL_liteDB/Program.cs
@@ -0,0 +1,36 @@
+﻿using LiteDB;
+using System;
+
+namespace noSQL_liteDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            using (var db = new LiteDatabase(@"db.db"))
+            {
+                var col = db.GetCollection<Auto>("Autos");
+
+                foreach (var item in col.Query().ToList())
+                {
+                    Console.WriteLine(item.Hersteller);
+                }
+                col.Insert(new Auto() { Hersteller = "Baudi",Modell="B66" });
+
+
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+    class Auto
+    {
+        public int Id { get; set; }
+        public string Hersteller { get; set; }
+        public string Modell { get; set; }
+
+    }
+}
